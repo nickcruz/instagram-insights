781d6ceee1d73bc85310fb9a3967c0f362ffbac5
Nick Cruz <cruz.nicolasbenjamin@gmail.com>
2026-04-08 11:50:58 -0400

    build: fix yarn build

--- apps/web/lib/env.ts
+++ apps/web/lib/env.ts
@@ -1,23 +1,4 @@
-import path from "node:path";
-
-import { loadEnvConfig } from "@next/env";
-
-let loaded = false;
-
-function loadWorkspaceEnv() {
-  if (loaded) {
-    return;
-  }
-
-  loadEnvConfig(process.cwd());
-  loadEnvConfig(path.resolve(process.cwd(), "../.."));
-  loaded = true;
-}
-
-loadWorkspaceEnv();
-
 export function getEnv(name: string) {
-  loadWorkspaceEnv();
   return process.env[name];
 }
 
